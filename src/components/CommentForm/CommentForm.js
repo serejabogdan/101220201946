6682dd67139f82b5b4c49887234d63dc20212c10
Hasst <sergey1112345@gmail.com>
2020-12-12 00:45:13 +0200

    added: add a comment with the Ctrl+Enter key combination

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -17,6 +17,25 @@ const month = {
 };
 
 const CommentForm = ({onAddComment}) => {
+  const downKeys = [];
+
+  const keysInclude = (e) => {
+    const keys = ['Enter', 'ControlLeft'];
+    downKeys.push(e.code);
+    if (downKeys.length === 2) {
+      for (let key of keys) {
+        if (!downKeys.includes(key)) {
+          return;
+        }
+      }
+    } else {
+      return;
+    }
+
+    textareaValue.trim() && onSubmitCommentData();
+    downKeys.length = 0;
+  };
+
   const [textareaValue, setState] = useState('');
 
   const onSubmitCommentData = () => {
@@ -30,7 +49,7 @@ const CommentForm = ({onAddComment}) => {
   };
 
   return (
-    <form className="comment-area">
+    <form className="comment-area" onKeyDown={keysInclude}>
       <textarea
         className="comment-input"
         name="comment"
