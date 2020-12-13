1604dfebdf2745f53d4fca614121e9ec2449051b
Hasst <sergey1112345@gmail.com>
2020-12-13 17:37:54 +0200

    changed: initialState and forming a comment date for filtering

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -1,20 +1,6 @@
 import React, {useState} from 'react';
+import {getStringDateNow} from '../../utils';
 import './CommentForm.scss';
-
-const month = {
-  0: 'января',
-  1: 'февраля',
-  2: 'марта',
-  3: 'апреля',
-  4: 'мая',
-  5: 'июня',
-  6: 'июля',
-  7: 'августа',
-  8: 'сентября',
-  9: 'октября',
-  10: 'ноября',
-  11: 'декабря'
-};
 
 const CommentForm = ({onAddComment}) => {
   const downKeys = [];
@@ -37,12 +23,11 @@ const CommentForm = ({onAddComment}) => {
   };
 
   const [textareaValue, setState] = useState('');
-
   const onSubmitCommentData = () => {
-    const date = new Date();
     const newComment = {
       personName: 'Сергей',
-      date: `${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()}`,
+      date: getStringDateNow(),
+      filterDate: Date.now(),
       content: textareaValue
     };
     onAddComment(newComment);
--- src/utils.js
+++ src/utils.js
@@ -8,22 +8,66 @@ export const isLocalStorage = (key) => {
   return !!getLocalStorage(key);
 };
 
+const month = {
+  0: 'января',
+  1: 'февраля',
+  2: 'марта',
+  3: 'апреля',
+  4: 'мая',
+  5: 'июня',
+  6: 'июля',
+  7: 'августа',
+  8: 'сентября',
+  9: 'октября',
+  10: 'ноября',
+  11: 'декабря'
+};
+
+export const getStringDateNow = () => {
+  const date = new Date();
+  return `${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()}`;
+};
+
 export const initialState = (key) => {
+  const filterDate = +new Date('09/13/2011');
+  const dateNow = Date.now();
   const comments = [
     {
       personName: 'Самуил',
       date: '13 октября 2011',
+      filterDate,
       content: 'Привет, Верунь! ниче себе ты крутая. фотка класс!!!!'
     },
     {
       personName: 'Лилия Семёновна',
       date: '14 октября 2011',
+      filterDate,
       content: `Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент,
   это и есть всемирно известный центр огранки алмазов и торговли бриллиантами?`
     },
     {
       personName: 'Лилия Семёновна',
       date: '14 октября 2011',
+      filterDate,
+      content: 'Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент?'
+    },
+    {
+      personName: 'Самуил',
+      date: getStringDateNow(),
+      filterDate: dateNow,
+      content: 'Привет, Верунь! ниче себе ты крутая. фотка класс!!!!'
+    },
+    {
+      personName: 'Лилия Семёновна',
+      date: getStringDateNow(),
+      filterDate: dateNow,
+      content: `Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент,
+  это и есть всемирно известный центр огранки алмазов и торговли бриллиантами?`
+    },
+    {
+      personName: 'Лилия Семёновна',
+      date: getStringDateNow(),
+      filterDate: dateNow,
       content: 'Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент?'
     }
   ];
